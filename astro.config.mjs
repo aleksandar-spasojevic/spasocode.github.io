d4202e8efda420f972c8af310d918ffaace92041
aleksandarspasojevic <aleksandar.spasojevic@outlook.com>
2026-01-10 18:01:52 +0100

    typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aleksandar-spasojevic.github.io',
-  base: 'spasocode.github.io',
+  base: '',
   vite: {
     plugins: [tailwindcss()],
   },
